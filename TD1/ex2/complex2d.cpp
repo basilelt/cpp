5bae3783d8e26f5632891f595650c87213fd4f34
BasileLT <blt_git@pm.me>
2025-11-06 12:56:45 +0000

    class Complex2D

--- TD1/ex2/complex2d.cpp
+++ TD1/ex2/complex2d.cpp
@@ -20,7 +20,126 @@ Complex2D::Complex2D(const Complex2D &complex) : re(complex.re), im(complex.im)
 {
 }
 
-Complex2D &Complex2D::operator+(const Complex2D &complex)
+Complex2D &Complex2D::operator+=(const Complex2D &complex)
 {
-    
+    re += complex.re;
+    im += complex.im;
+    return *this;
+}
+
+Complex2D Complex2D::operator+(const Complex2D &complex) const
+{
+    return Complex2D(re + complex.re, im + complex.im);
+}
+
+Complex2D &Complex2D::operator-=(const Complex2D &complex)
+{
+    re -= complex.re;
+    im -= complex.im;
+    return *this;
+}
+
+Complex2D Complex2D::operator-(const Complex2D &complex) const
+{
+    return Complex2D(re - complex.re, im - complex.im);
+}
+
+Complex2D &Complex2D::operator*=(const Complex2D &complex)
+{
+    double new_re = re * complex.re - im * complex.im;
+    double new_im = re * complex.im + im * complex.re;
+    re = new_re;
+    im = new_im;
+    return *this;
+}
+
+Complex2D Complex2D::operator*(const Complex2D &complex) const
+{
+    double new_re = re * complex.re - im * complex.im;
+    double new_im = re * complex.im + im * complex.re;
+    return Complex2D(new_re, new_im);
+}
+
+Complex2D &Complex2D::operator/=(const Complex2D &complex)
+{
+    double denominator = complex.re * complex.re + complex.im * complex.im;
+    double new_re = (re * complex.re + im * complex.im) / denominator;
+    double new_im = (im * complex.re - re * complex.im) / denominator;
+    re = new_re;
+    im = new_im;
+    return *this;
+}
+
+Complex2D Complex2D::operator/(const Complex2D &complex) const
+{
+    double denominator = complex.re * complex.re + complex.im * complex.im;
+    double new_re = (re * complex.re + im * complex.im) / denominator;
+    double new_im = (im * complex.re - re * complex.im) / denominator;
+    return Complex2D(new_re, new_im);
+}
+
+Complex2D &Complex2D::operator=(const Complex2D &complex)
+{
+    if (&complex != this)
+    {
+        re = complex.re;
+        im = complex.im;
+    }
+    return *this;
+}
+
+bool Complex2D::operator<(const Complex2D &complex) const
+{
+    return (re < complex.re) || (re == complex.re && im < complex.im);
+}
+
+bool Complex2D::operator>(const Complex2D &complex) const
+{
+    return (re > complex.re) || (re == complex.re && im > complex.im);
+}
+
+bool Complex2D::operator<=(const Complex2D &complex) const
+{
+    return (re < complex.re) || (re == complex.re && im <= complex.im);
+}
+
+bool Complex2D::operator>=(const Complex2D &complex) const
+{
+    return (re > complex.re) || (re == complex.re && im >= complex.im);
+}
+
+bool Complex2D::operator==(const Complex2D &complex) const
+{
+    return (re == complex.re) && (im == complex.im);
+}
+
+bool Complex2D::operator!=(const Complex2D &complex) const
+{
+    return !(*this == complex);
+}
+
+double Complex2D::getRe() const
+{
+    return re;
+}
+
+double Complex2D::getIm() const
+{
+    return im;
+}
+
+void Complex2D::setRe(double _re)
+{
+    re = _re;
+}
+
+void Complex2D::setIm(double _im)
+{
+    im = _im;
+}
+
+ostream &Complex2D::printOn(ostream &os) const
+{
+    os << re << " + " << im << "i";
+    return os;
 }
--- TD1/ex2/complex2d.hpp
+++ TD1/ex2/complex2d.hpp
@@ -3,8 +3,6 @@
 
 #include <string>
 
-using namespace std;
-
 class Complex2D
 {
 public:
@@ -14,17 +12,28 @@ public:
     Complex2D(const Complex2D &);
     virtual ~Complex2D() = default;
 
-    virtual Complex2D &operator+(const Complex2D &);
-    virtual Complex2D &operator-(const Complex2D &);
-    virtual Complex2D &operator*(const Complex2D &);
-    virtual Complex2D &operator/(const Complex2D &);
-    virtual Complex2D &operator<(const Complex2D &);
-    virtual Complex2D &operator>(const Complex2D &);
+    virtual Complex2D &operator+=(const Complex2D &);
+    virtual Complex2D operator+(const Complex2D &) const;
+    virtual Complex2D &operator-=(const Complex2D &);
+    virtual Complex2D operator-(const Complex2D &) const;
+    virtual Complex2D &operator*=(const Complex2D &);
+    virtual Complex2D operator*(const Complex2D &) const;
+    virtual Complex2D &operator/=(const Complex2D &);
+    virtual Complex2D operator/(const Complex2D &) const;
+    virtual Complex2D &operator=(const Complex2D &);
+    virtual bool operator<(const Complex2D &) const;
+    virtual bool operator>(const Complex2D &) const;
+    virtual bool operator<=(const Complex2D &) const;
+    virtual bool operator>=(const Complex2D &) const;
+    virtual bool operator==(const Complex2D &) const;
+    virtual bool operator!=(const Complex2D &) const;
 
     virtual double getRe() const;
     virtual double getIm() const;
-    virtual void setRe();
-    virtual void setIm();
+    virtual void setRe(double);
+    virtual void setIm(double);
+
+    virtual std::ostream& printOn(std::ostream&) const;
 
 private:
     double re, im;
