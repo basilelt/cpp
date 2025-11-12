024bd27c2ea6cde9372d67f7e5e9bc990bae1361
BasileLT <blt_git@pm.me>
2025-11-12 12:22:06 +0000

    Implement LinearSearch

--- TD2/ex1/src/main.cpp
+++ TD2/ex1/src/main.cpp
@@ -4,10 +4,10 @@
 int main()
 {
     std::vector<int> v1 = {1, 2, 3, 4, 5};
-    LinearSearch algo;
+    LinearSearch linear;
     int target = 3;
-    int result = algo.search(v1, target);
+    int result = linear.search(v1, target);
 
-    algo.displaySearchResults(std::cout, result, target);
+    linear.displaySearchResults(std::cout, result, target);
     return 0;
 }
