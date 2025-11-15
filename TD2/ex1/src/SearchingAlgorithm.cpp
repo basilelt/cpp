72b5f027ef1b80ac196d133f981c13c13a5c2c07
BasileLT <blt_git@pm.me>
2025-11-15 14:52:58 +0000

    commented out now virtual pure function

--- TD2/ex1/src/SearchingAlgorithm.cpp
+++ TD2/ex1/src/SearchingAlgorithm.cpp
@@ -16,21 +16,22 @@ SearchingAlgorithm::~SearchingAlgorithm()
 {
 }
 
-int SearchingAlgorithm::search(std::vector<int> v1, int target)
-{
-    /* Recherche linéaire pour tester */
-    totalSearch++; /* Incrémente le nombre de recherches pour toutes instances de la classe */
-    std::vector<int>::iterator it = v1.begin();
-    for (; it != v1.end(); ++it)
-    {
-        numberComparisons++; /* Incrémente le nombre de comparaisons pour cette instance */
-        if (*it == target)
-        {
-            return std::distance(v1.begin(), it);
-        }
-    }
-    return -1;
-}
+// Used for testing purposes, commented now
+// int SearchingAlgorithm::search(std::vector<int> v1, int target)
+// {
+//     /* Recherche linéaire pour tester */
+//     totalSearch++; /* Incrémente le nombre de recherches pour toutes instances de la classe */
+//     std::vector<int>::iterator it = v1.begin();
+//     for (; it != v1.end(); ++it)
+//     {
+//         numberComparisons++; /* Incrémente le nombre de comparaisons pour cette instance */
+//         if (*it == target)
+//         {
+//             return std::distance(v1.begin(), it);
+//         }
+//     }
+//     return -1;
+// }
 
 std::ostream &SearchingAlgorithm::displaySearchResults(std::ostream &os, int results, int target) const
 {
