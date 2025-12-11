d633593fbabfd7b93923d2d778e7809565a8ceab
BasileLT <blt_git@pm.me>
2025-12-11 09:31:08 +0000

    class card

--- TP1/pokemon/include/Card.hpp
+++ TP1/pokemon/include/Card.hpp
@@ -6,6 +6,13 @@
 class Card : public Base
 {
 public:
+    Card();             /* constructeur par défaut */
+    Card(std::string);  /* constructeur avec paramètres */
+    Card(const Card &); /* constructeur de copie */
+    virtual ~Card();    /* destructeur */
+
+    virtual Card &operator=(const Card &); /* opérateur d'affectation */
+
     virtual std::ostream &diplayInfo(std::ostream &os) const = 0; /* Méthode virtuelle pure */
 
 protected:
--- TP1/pokemon/src/Card.cpp
+++ TP1/pokemon/src/Card.cpp
@@ -1,1 +1,41 @@
 #include "Card.hpp"
+#include <iostream>
+
+/* Constructeur par défaut */
+Card::Card() : Card(0, 0)
+{
+}
+
+/* Constructeur avec paramètres */
+Card::Card(int _x, int _y) : x(_x), y(_y)
+{
+}
+
+/* Constructeur de copie */
+Card::Card(const Card &p) : Card(p.x, p.y)
+{
+}
+
+/* Destructeur */
+Card::~Card()
+{
+}
+
+/* Opérateur d'affectation */
+Card &Card::operator=(const Card &p)
+{
+    if (&p != this)
+    {
+        x = p.x;
+        y = p.y;
+    }
+    return *this;
+}
+
+/* Surcharge de l'opérateur d'affichage */
+std::ostream &Card::PrintOn(std::ostream &os) const
+{
+    os << "Card(";
+    os << x << ", " << y << ")";
+    return os;
+}
